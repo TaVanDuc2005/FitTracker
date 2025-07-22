68f18fb853551537b0f854755b3e37a5ee002e4d
quangminhbmv <quangmin3009@gmail.com>
2025-07-22 14:48:20 +0700

    Push gium Luan

--- fittracker_source/lib/main.dart
+++ fittracker_source/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Screens/active_screen/Journal/journal.dart';
+import 'Screens/initial_screen/Page1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'FitTracker',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green, useMaterial3: true),
-      home: const JournalScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
